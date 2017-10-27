daa5ff620e838dabc483d8e243d4e1c13969f26c
jhta26 <hsujasonf@gmail.com>
2017-10-27 10:20:47 -0700

    took out shit

--- encrypt.js
+++
@@ -1,9 +0,0 @@
-
-function encrypt(string){
-	return string
-	.toString()
-	.split('')
-	.map((a,i)=>a=string.charCodeAt(i)).reverse()
-	.map((a,i)=>a+(i*26)).map((a,i)=>~~a-(i-12)*2+'')
-	.reduce((a,b)=>a+b)
-}
