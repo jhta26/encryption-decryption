ef588f78eb5b03146cfd114dd9ae169ce78b0f29
jhta26 <hsujasonf@gmail.com>
2017-10-27 09:53:48 -0700

    encrypt

---
+++ encrypt.js
@@ -0,0 +1,9 @@
+
+function encrypt(string){
+	return string
+	.toString()
+	.split('')
+	.map((a,i)=>a=string.charCodeAt(i)).reverse()
+	.map((a,i)=>a+(i*26)).map((a,i)=>~~a-(i-12)*2+'')
+	.reduce((a,b)=>a+b)
+}
