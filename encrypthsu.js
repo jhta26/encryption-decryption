782480aa06821c992e2a25df00086d13b146d03e
jhta26 <hsujasonf@gmail.com>
2017-10-27 10:18:48 -0700

    diff

--- encrypthsu.js
+++ encrypthsu.js
@@ -1,8 +1,8 @@
 function encrypt(string){
-	return string
-	.toString()
-	.split('')
-	.map((a,i)=>a=string.charCodeAt(i)).reverse()
-	.map((a,i)=>a+(i*26)).map((a,i)=>~~a-(i-12)*2+'')
-	.reduce((a,b)=>a+b)
+	return string+'HELLO'
 }
+
+function decrypt(string){
+	return string.substr(0,string.length-5)
+}
+
